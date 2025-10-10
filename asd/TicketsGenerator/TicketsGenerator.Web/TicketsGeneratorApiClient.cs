391209ea8dcde1c6e2a7a1271e726be2bd6be4e4
Andrey Akulov <akulov.a@bookcentre.ru>
2025-10-10 16:38:33 +0300

    Business logic is ready

--- asd/TicketsGenerator/TicketsGenerator.Web/TicketsGeneratorApiClient.cs
+++ asd/TicketsGenerator/TicketsGenerator.Web/TicketsGeneratorApiClient.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http.Extensions;
 using System.Net.Http;
+using System.Globalization;
 
 
 namespace TicketsGenerator.Web;
@@ -13,11 +14,11 @@ public class TicketsGeneratorApiClient(HttpClient httpClient)
         var queryBuilder = new QueryBuilder();
 
         uriBuilder.Path = "api/v1/tickets-generator";
-        queryBuilder.Add("PersonsCount", personsCount.ToString());
+        queryBuilder.Add("PersonsCount", personsCount.ToString(CultureInfo.InvariantCulture));
         if (visitDate != null)
-            queryBuilder.Add("VisitDate", visitDate.ToString()!);
+            queryBuilder.Add("VisitDate", visitDate.Value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
         if (saleDate != null)
-            queryBuilder.Add("SaleDate", saleDate.ToString()!);
+            queryBuilder.Add("SaleDate", saleDate.Value.ToString("o", CultureInfo.InvariantCulture));
 
         uriBuilder.Query = queryBuilder.ToString();
         var response = await httpClient.GetAsync(uriBuilder.Uri, cancellationToken).ConfigureAwait(false);
