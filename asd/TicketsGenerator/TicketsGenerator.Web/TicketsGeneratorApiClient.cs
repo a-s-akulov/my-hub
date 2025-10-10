591bcf6dd7bc8f513c4a1da4916331a0bd628a1d
Andrey Akulov <akulov.a@bookcentre.ru>
2025-10-10 15:48:25 +0300

    фыв

--- asd/TicketsGenerator/TicketsGenerator.Web/TicketsGeneratorApiClient.cs
+++ asd/TicketsGenerator/TicketsGenerator.Web/TicketsGeneratorApiClient.cs
@@ -7,13 +7,17 @@ namespace TicketsGenerator.Web;
 
 public class TicketsGeneratorApiClient(HttpClient httpClient)
 {
-    public async Task<HttpContent> GetTickets(int personsCount, CancellationToken cancellationToken = default)
+    public async Task<HttpContent> GetTickets(int personsCount, DateOnly? visitDate, DateTimeOffset? saleDate, CancellationToken cancellationToken = default)
     {
         var uriBuilder = new UriBuilder(httpClient.BaseAddress);
         var queryBuilder = new QueryBuilder();
 
         uriBuilder.Path = "api/v1/tickets-generator";
         queryBuilder.Add("PersonsCount", personsCount.ToString());
+        if (visitDate != null)
+            queryBuilder.Add("VisitDate", visitDate.ToString()!);
+        if (saleDate != null)
+            queryBuilder.Add("SaleDate", saleDate.ToString()!);
 
         uriBuilder.Query = queryBuilder.ToString();
         var response = await httpClient.GetAsync(uriBuilder.Uri, cancellationToken).ConfigureAwait(false);
