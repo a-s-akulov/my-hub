06021ce84659a4f4ab9a3f696a10cdc96b2fa77d
Andrey Akulov <akulov.a@bookcentre.ru>
2025-10-10 14:42:32 +0300

    Draw TicketIdBarcode

--- TicketsGenerator/TicketsGenerator/TicketsGeneratorServices.Api/RequestHandlers/V1/TicketsGenerator/GetTicketsV1RequestHandler.cs
+++ TicketsGenerator/TicketsGenerator/TicketsGeneratorServices.Api/RequestHandlers/V1/TicketsGenerator/GetTicketsV1RequestHandler.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
+using PdfSharp;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
 using PdfSharp.Pdf.IO;
 using QRCoder;
-using System.Diagnostics;
+using SkiaSharp;
 using Svg;
+using System.Diagnostics;
 using TicketsGeneratorServices.Api.DTO.V1.TicketsGenerator;
 using TicketsGeneratorServices.Api.RequestHandlers.Base;
 using TicketsGeneratorServices.Common.Services.TicketsGeneratorStorageService;
@@ -83,6 +85,7 @@ public class GetTicketsV1RequestHandler : RequestHandlerBase<ITicketsGeneratorSt
         SetOrderId(page, orderId);
         SetTicketId(page, ticketId);
         SetTicketIdQr(page, ticketId);
+        SetTicketIdBarcode(page, ticketId);
         SetVisitDate(page, visitDate);
         SetSaleDate(page, saleDate);
         
@@ -102,7 +105,7 @@ public class GetTicketsV1RequestHandler : RequestHandlerBase<ITicketsGeneratorSt
         var areaRect = new XRect(110.4, pdfPage.Height - 29.3, 160, 10);
 
         gfx.DrawRectangle(XBrushes.White, areaRect);
-        gfx.DrawString(orderId, font, XBrushes.Red, areaRect, XStringFormats.CenterLeft);
+        gfx.DrawString(orderId, font, XBrushes.Black, areaRect, XStringFormats.CenterLeft);
     }
 
 
@@ -115,7 +118,7 @@ public class GetTicketsV1RequestHandler : RequestHandlerBase<ITicketsGeneratorSt
 
         gfx.RotateTransform(-90);
         gfx.DrawRectangle(XBrushes.White, areaRect);
-        gfx.DrawString(ticketId.ToString(), font, XBrushes.Red, areaRect, XStringFormats.Center);
+        gfx.DrawString(ticketId.ToString(), font, XBrushes.Black, areaRect, XStringFormats.Center);
     }
 
 
@@ -141,6 +144,33 @@ public class GetTicketsV1RequestHandler : RequestHandlerBase<ITicketsGeneratorSt
     }
 
 
+    private static void SetTicketIdBarcode(PdfPage pdfPage, int ticketId)
+    {
+        using var gfx = XGraphics.FromPdfPage(pdfPage);
+
+        var areaRect = new XRect(-150, 337, 122, 70);
+
+
+        using var barcode = new BarcodeStandard.Barcode()
+        {
+            IncludeLabel = false
+        };
+        using var barcodeEncoded = barcode.Encode(BarcodeStandard.Type.Code128A, ticketId.ToString(), SKColors.Black, SKColors.White, 140, (int)areaRect.Height);
+        using var imageEncoded = barcodeEncoded.Encode();
+
+        using var qrCodeStream = new MemoryStream();
+        imageEncoded.SaveTo(qrCodeStream);
+        qrCodeStream.Seek(0, SeekOrigin.Begin);
+
+        using var image = XImage.FromStream(qrCodeStream);
+        image.Interpolate = false;
+
+        gfx.RotateTransform(-90);
+        gfx.DrawRectangle(XBrushes.White, areaRect);
+        gfx.DrawImage(image, areaRect);
+    }
+
+
     private static void SetVisitDate(PdfPage pdfPage, DateOnly visitDate)
     {
         using var gfx = XGraphics.FromPdfPage(pdfPage);
@@ -149,7 +179,7 @@ public class GetTicketsV1RequestHandler : RequestHandlerBase<ITicketsGeneratorSt
         var areaRect = new XRect(138.2, pdfPage.Height - 99.1, 50, 10);
 
         gfx.DrawRectangle(XBrushes.White, areaRect);
-        gfx.DrawString(visitDate.ToString("dd.MM.yyyy"), font, XBrushes.Red, areaRect, XStringFormats.CenterLeft);
+        gfx.DrawString(visitDate.ToString("dd.MM.yyyy"), font, XBrushes.Black, areaRect, XStringFormats.CenterLeft);
     }
 
 
@@ -161,7 +191,7 @@ public class GetTicketsV1RequestHandler : RequestHandlerBase<ITicketsGeneratorSt
         var areaRect = new XRect(107, pdfPage.Height - 48.2, 160, 15);
 
         gfx.DrawRectangle(XBrushes.White, areaRect);
-        gfx.DrawString(saleDate.ToString("f"), font, XBrushes.Red, areaRect, XStringFormats.CenterLeft);
+        gfx.DrawString(saleDate.ToString("f"), font, XBrushes.Black, areaRect, XStringFormats.CenterLeft);
     }
 
     #endregion Методы
